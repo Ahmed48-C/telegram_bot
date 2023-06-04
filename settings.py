57862619954694e36372032b5a655603c0aba89e
Ahmed48-C <85570965+Ahmed48-C@users.noreply.github.com>
2023-06-04 18:56:46 +0800

    reverse back settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 from typing import Final
 
-TOKEN: Final = '5711672688:AAET8nIq3k8jh8rFjPWPxNTaMmR_UUyRfAE'
-BOT_USERNAME: Final = '@AhmedTut1_bot'
+TOKEN: Final = 'your_token'
+BOT_USERNAME: Final = 'your_name'
