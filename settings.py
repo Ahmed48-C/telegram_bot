797a13e15d58ef2c756d94e8600e22a5c17ebd9b
Ahmed48-C <85570965+Ahmed48-C@users.noreply.github.com>
2023-06-04 18:54:41 +0800

    added fibonacci sequence command

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 from typing import Final
 
-TOKEN: Final = 'your_token'
-BOT_USERNAME: Final = 'your_username'
+TOKEN: Final = '5711672688:AAET8nIq3k8jh8rFjPWPxNTaMmR_UUyRfAE'
+BOT_USERNAME: Final = '@AhmedTut1_bot'
